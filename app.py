4864be884ea2a83c3f36c5b6f5520522b042053c
Doai Tran <doai.tran@tyme.com>
2022-05-13 23:49:46 +0700

    update app

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
+    consultant = db.Column(db.String(200))
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, comments):
+    def __init__(self, customer, consultant, rating, comments):
         self.customer = customer
-        self.dealer = dealer
+        self.consultant = consultant
         self.rating = rating
         self.comments = comments
 
@@ -42,17 +42,17 @@ def index():
 def submit():
     if request.method == 'POST':
         customer = request.form['customer']
-        dealer = request.form['dealer']
+        consultant = request.form['consultant']
         rating = request.form['rating']
         comments = request.form['comments']
-        # print(customer, dealer, rating, comments)
-        if customer == '' or dealer == '':
+        # print(customer, consultant, rating, comments)
+        if customer == '' or consultant == '':
             return render_template('index.html', message='Please enter required fields')
         if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
+            data = Feedback(customer, consultant, rating, comments)
             db.session.add(data)
             db.session.commit()
-            send_mail(customer, dealer, rating, comments)
+            # send_mail(customer, consultant, rating, comments)
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback')
 
